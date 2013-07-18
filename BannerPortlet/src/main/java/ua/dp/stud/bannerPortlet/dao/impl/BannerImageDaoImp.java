e4c7b23af02ae5cc68af54311a58ac54ef16e90f
lapkofear <lapkofear@gmail.com>
2013-07-18 23:18:27 +0300

    code freeze

---
+++ BannerPortlet/src/main/java/ua/dp/stud/bannerPortlet/dao/impl/BannerImageDaoImp.java
@@ -0,0 +1,66 @@
+package ua.dp.stud.bannerPortlet.dao.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ua.dp.stud.StudPortalLib.model.ImageImpl;
+import ua.dp.stud.bannerPortlet.dao.BannerImageDao;
+import ua.dp.stud.bannerPortlet.model.BannerImage;
+
+import java.util.Collection;
+
+/**
+ * Class is responsible for working with the database
+ *
+ * @author Vladislav Pikus
+ */
+@Repository("bannerDao")
+public class BannerImageDaoImp implements BannerImageDao {
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    private Session getSession() {
+        return this.sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public BannerImage getBannerImageById(Integer id) {
+        return (BannerImage) getSession().get(BannerImage.class, id);
+    }
+
+    @Override
+    public void addBannerImage(BannerImage banner) {
+        getSession().save(banner);
+    }
+
+    @Override
+    public void deleteBannerImage(Integer id) {
+        BannerImage banner = (BannerImage) getSession().get(BannerImage.class, id);
+        ImageImpl image = banner.getMainImage();
+        if (image != null) {
+            getSession().delete(image);
+        }
+        getSession().delete(banner);
+    }
+
+    @Override
+    public void updateBannerImage(BannerImage banner) {
+        getSession().update(banner);
+    }
+
+    @Override
+    public Collection<BannerImage> getAll() {
+        return getSession().createCriteria(BannerImage.class).list();
+    }
+
+    @Override
+    public BannerImage getByURL(String url) {
+        return (BannerImage) getSession().createCriteria(BannerImage.class).add(Restrictions.eq("url", url)).uniqueResult();
+    }
+}
---
+++ BannerPortlet/src/main/java/ua/dp/stud/bannerPortlet/service/impl/BannerImageServiceImpl.java
@@ -0,0 +1,67 @@
+package ua.dp.stud.bannerPortlet.service.impl;
+
+/**
+ * @author Vladislav Pikus
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ua.dp.stud.bannerPortlet.dao.BannerImageDao;
+import ua.dp.stud.bannerPortlet.model.BannerImage;
+import ua.dp.stud.bannerPortlet.service.BannerImageService;
+
+import java.util.Collection;
+
+/**
+ * @author Vladislav Pikus
+ */
+@Service("bannerImageService")
+public class BannerImageServiceImpl implements BannerImageService {
+
+    private BannerImageDao dao;
+
+    @Transactional(readOnly = false)
+    @Autowired
+    @Qualifier(value = "bannerDao")
+    public void setDao(BannerImageDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public BannerImage getBannerImageById(Integer id) {
+        return dao.getBannerImageById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void addBannerImage(BannerImage banner) {
+        dao.addBannerImage(banner);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void deleteBannerImage(BannerImage banner) {
+        dao.deleteBannerImage(banner.getId());
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void updateBannerImage(BannerImage banner) {
+        dao.updateBannerImage(banner);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Collection<BannerImage> getAll() {
+        return dao.getAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public BannerImage getByURL(String url) {
+        return dao.getByURL(url);
+    }
+}
