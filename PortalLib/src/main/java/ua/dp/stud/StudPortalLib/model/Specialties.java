f6a3960adc9d34a218db7aa33498e472d61b0936
vlablack <pvladislavs@gmail.com>
2013-07-21 22:05:28 +0300

    add lost field adn some one changes

--- PortalLib/src/main/java/ua/dp/stud/StudPortalLib/model/Specialties.java
+++ PortalLib/src/main/java/ua/dp/stud/StudPortalLib/model/Specialties.java
@@ -1,19 +1,9 @@
 package ua.dp.stud.StudPortalLib.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
 
 /**
- *
  * @author Ольга
  */
 @Entity
@@ -22,6 +12,7 @@ public class Specialties implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     public Integer getId() {
         return id;
     }
@@ -33,16 +24,18 @@ public class Specialties implements Serializable {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "nameOfFaculties")
     private Faculties nameOfFaculties;
-    @Column(name = "nameOfSpecialties")
-    private String nameOfSpecialties;
 
-   public void setNameOfFaculties(Faculties nameOfFaculties) {
+    public void setNameOfFaculties(Faculties nameOfFaculties) {
         this.nameOfFaculties = nameOfFaculties;
     }
 
     public Faculties getNameOfFaculties() {
         return nameOfFaculties;
     }
+
+    @Column(name = "nameOfSpecialties")
+    private String nameOfSpecialties;
+
 
     public void setNameOfSpecialties(String nameOfSpecialties) {
         this.nameOfSpecialties = nameOfSpecialties;
